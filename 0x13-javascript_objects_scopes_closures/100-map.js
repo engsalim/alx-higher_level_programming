0bcfb80ef1fc8fc4e70bd29824507d5c6fbbeb7e
engsalim <39193592+engsalim@users.noreply.github.com>
2022-10-10 22:33:01 +0300

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 
-const arr = require('./100-data').list;
+// Write a script that imports an array and computes a new array.
 
-console.log(arr);
-console.log(arr.map((x, idx) => x * idx));
+const arrayInit = require('./100-data').list;
+
+const newArray = arrayInit.map(function (element, index) {
+  return (element * index);
+});
+
+console.log(arrayInit);
+console.log(newArray);
