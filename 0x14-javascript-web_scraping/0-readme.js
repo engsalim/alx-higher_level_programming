f81ed63cb267b4e4b8f98f34c369a7a28cd1fd50
engsalim <39193592+engsalim@users.noreply.github.com>
2022-11-15 23:29:42 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf-8', function (err, result) {
- if (err) {
-  console.log(err);
- } else {
-  console.log(result);
- }
+fs.readFile(process.argv[2], 'utf8', (err, content) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(content);
+  }
 });
