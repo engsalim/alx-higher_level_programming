1995c0a61525fa48df090c2d38f1d0776af03989
engsalim <39193592+engsalim@users.noreply.github.com>
2022-11-15 23:42:18 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
- if (error) {
-  console.error(error);
- }
- console.log(JSON.parse(body).title);
+let endPoint = 'http://swapi.co/api/films/' + process.argv[2];
+request({ url: endPoint, methods: 'GET' }, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(body && JSON.parse(body).title);
+  }
 });
