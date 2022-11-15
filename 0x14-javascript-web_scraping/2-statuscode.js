79a4cb8b4b0d241a7656890ff4dc78f550752698
engsalim <39193592+engsalim@users.noreply.github.com>
2022-11-15 23:41:04 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-request(process.argv[2], function (error, response) {
- if (error) {
-  console.error(error);
- }
- console.log('code:', response && response.statusCode);
+request({ url: process.argv[2], methods: 'GET' }, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
 });
